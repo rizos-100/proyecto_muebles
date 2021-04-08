9a1afdd7c8a8e5d054c8aea271ec52367046b1ed
JessiArcos <81044848+JessiArcos@users.noreply.github.com>
2021-04-08 17:45:23 -0500

    Categoría FontEnd
    Se modifica la vista de categorías.

--- flask_muebles_mysql/project/static/MyJS/categoria.js
+++ flask_muebles_mysql/project/static/MyJS/categoria.js
@@ -7,12 +7,26 @@ function decidirCategoria(){
 }
 
 function agregarCategoria(){
-    if(validarCategoria()){
+    var err = validarCategoria();
+    if(err == "ok"){
+        alert("Aqui");
         var data = {
             nombre: $('#txtNombreCategoria').val(),
             descripcion: $('#txtDescCategoria').val()
         };
-        $.ajax(
+
+        swal({
+            title: "¿Deseas continuar?",
+            text: "Por favor, confirma que deseas agregar el registro.",
+            type: "info",
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: "#4caf50",
+            confirmButtonText: "Si, agregar",
+            closeOnConfirm: false,
+            showLoaderOnConfirm: true
+        }, function () {
+            $.ajax(
                 {
                     type: "POST",
                     url: "/addCategoria",
@@ -21,24 +35,38 @@ function agregarCategoria(){
                 })
                 .done(
                         function (data) {
-                            Swal.fire('Movimiento realizado', 'Categoria guardada con exito', 'success');
+                            swal("Correcto", "Categoría guardada con exíto. ", "success");
                             setTimeout(function(){location.href ="/getAllCategoriasActivas";;},2000)
                         }
                 );
+        });
     }else{
-        Swal.fire('Ha ocurrido un error', 'No debes dejar campos de la categoria vacios', 'error');
+        //Swal.fire('Ha ocurrido un error', 'No debes dejar campos de la categoria vacios', 'error');
+        showNotification("bg-black", err, "bottom", "left", "", "");
     }
     
 }
 
 function modificarCategoria(){
-    if(validarCategoria()){
+    var err = validarCategoria();
+    if(err == "ok"){
         var data = {
             id: $('#txtIdCategoria').val(),
             nombre: $('#txtNombreCategoria').val(),
             descripcion: $('#txtDescCategoria').val()
         };
-        $.ajax(
+        swal({
+            title: "¿Deseas continuar?",
+            text: "Por favor, confirma que deseas modificar el registro.",
+            type: "info",
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: "#4caf50",
+            confirmButtonText: "Si, modificar",
+            closeOnConfirm: false,
+            showLoaderOnConfirm: true
+        }, function () {
+            $.ajax(
                 {
                     type: "POST",
                     url: "/updateCategoria",
@@ -47,12 +75,14 @@ function modificarCategoria(){
                 })
                 .done(
                         function (data) {
-                            Swal.fire('Movimiento realizado', 'Categoria modificada con exito', 'success');
+                            swal('Movimiento realizado', 'Categoría modificada con exíto', 'success');
                             setTimeout(function(){location.href ="/getAllCategoriasActivas";;},2000)
                         }
                 );
+        });
     }else{
-        Swal.fire('Ha ocurrido un error', 'No debes dejar campos de la categoria vacios', 'error');
+        //Swal.fire('Ha ocurrido un error', 'No debes dejar campos de la categoria vacios', 'error');
+        showNotification("bg-black", err, "bottom", "left", "", "");
     }
     
 }
@@ -81,44 +111,93 @@ function detalleCategoria(idC){
 }
 
 function eliminarCategoria(idC){
-    Swal.fire({
-        title: '¿Seguro que deseas eliminarlo?',
-        text: "Es posible que no puedas deshacerlo!",
-        icon: 'warning',
+    var data = {
+        id: idC
+    };
+    swal({
+        title: "¿Deseas continuar?",
+        text: "Por favor, confirma que deseas eliminar el registro.",
+        type: "info",
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, Eliminar!',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.value) {
-            var data = {
-                id: idC
-            };
-            $.ajax(
-                    {
-                        type: "POST",
-                        url: "/deleteCategoria",
-                        async: true,
-                        data: data
-                    })
-                    .done(
-                            function (data) {
-                                Swal.fire('Movimiento realizado', 'Categoria eliminada con exito', 'success');
-                                setTimeout(function(){location.href ="/getAllCategoriasInactivas";},2000)
-                            }
-                    );
-        }
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, eliminar",
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true
+    }, function () {
+        $.ajax(
+            {
+                type: "POST",
+                url: "/deleteCategoria",
+                async: true,
+                data: data
+            })
+            .done(
+                    function (data) {
+                        swal('Movimiento realizado', 'Categoría eliminada con exíto.', 'success');
+                        setTimeout(function(){location.href ="/getAllCategoriasInactivas";},2000)
+                    }
+            );
     });
     
 }
 
 function validarCategoria(){
     if($('#txtNombreCategoria').val() == ""){
-        return false;
+        return "Por favor, indica el nombre. ";
     }
+
     if($('#txtDescCategoria').val() == ""){
-        return false;
+        return "Por favor, indica la descripción";;
     }
-    return true;
+
+    return "ok";
+}
+
+
+
+/* Funciones para notificaciones */
+
+function showNotification(colorName, text, placementFrom, placementAlign, animateEnter, animateExit) {
+    if (colorName === null || colorName === '') {
+        colorName = 'bg-black';
+    }
+    if (text === null || text === '') {
+        text = 'Turning standard Bootstrap alerts';
+    }
+    if (animateEnter === null || animateEnter === '') {
+        animateEnter = 'animated fadeInDown';
+    }
+    if (animateExit === null || animateExit === '') {
+        animateExit = 'animated fadeOutUp';
+    }
+    var allowDismiss = true;
+
+    $.notify({
+        message: text
+    },
+            {
+                type: colorName,
+                allow_dismiss: allowDismiss,
+                newest_on_top: true,
+                timer: 1000,
+                placement: {
+                    from: placementFrom,
+                    align: placementAlign
+                },
+                animate: {
+                    enter: animateEnter,
+                    exit: animateExit
+                },
+                template: '<div data-notify="container" class="bootstrap-notify-container alert alert-dismissible {0} ' + (allowDismiss ? "p-r-35" : "") + '" role="alert">' +
+                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                        '<span data-notify="icon"></span> ' +
+                        '<span data-notify="title">{1}</span> ' +
+                        '<span data-notify="message">{2}</span>' +
+                        '<div class="progress" data-notify="progressbar">' +
+                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                        '</div>' +
+                        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                        '</div>'
+            });
 }
